206083a219120225098658ab6eb6eba06d8b9fe1
sajaalghoul <sajanabeel2@gmail.com>
2018-07-25 11:57:40 +0300

    final project

--- Final Project/app.py
+++ Final Project/app.py
@@ -1,97 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/hello")
-# def hello():
-# 	return "Hello World!"
-
-
-# @app.route("/goodbye")
-# def goodbye():
-# 	return "Goodbye World!"
-
-# @app.route("/")
-# def name():
-# 	return "My name is saja!"
-
-# @app.route("/saso")
-# def saso():
-# 	return "My nikename is soso!"
-
-
-# if __name__=="__main__":
-# 	app.run(port = 4000, debug=True)
-###############################3
-# from flask import Flask
-# import random
-
-# app = Flask(__name__)
-
-# @app.route("/quote")
-# def quote():
-# 	a=["Hello!","my name is saja","I am 16 years","I  love the black color"]
-# 	x=random.randint(0,len(a)-1)
-# 	# x=(random.choice(a))
-
-# 	return a[x]
-
-# if __name__=="__main__":
-# 	app.run(port = 4000, debug=True)	
-###############################3
-# from flask import Flask,render_template
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def website():
-# 	return  render_template("personal.html")
-
-# @app.route("/wow.html")
-# def a():
-# 	return render_template("wow.html")
-
-
-# if __name__=="__main__":
-# 	app.run(debug=True)
-#####################################
-
-# from flask import Flask, render_template,request
-# app = Flask(__name__)
-
-
-# @app.route("/personal")
-# def website():
-# 	return render_template("personal.html",name="Welcom to my Home Page")
-
-# @app.route("/wow.html")
-# def a():
-# 	return render_template("wow.html")
-
-# @app.route("/")
-# def homaa():
-# 	return render_template("home.html",name="Welcom to my Home Page")
-
-# @app.route("/form")
-# def forma():
-# 	return render_template("forms.html",name="Welcom to my Home Page")
-
-# @app.route("/char")
-# def char():
-# 	return render_template("characters.html",name="Welcom to my Home Page")
-
-# @app.route("/submit", methods=["POST"])
-# def submit():
-# 	myfirstname=request.form["firstName"]
-# 	mylastname=request.form["lastName"]
-# 	mypassword=request.form["Password"]
-
-# 	return render_template("submit.html", nameOne=myfirstname, nameTwo=mylastname, password=mypassword)
-	
-# if __name__=="__main__":
-# 	app.run(port=3000,debug=True)
-####################################
-
 from flask import Flask, render_template,request
 app = Flask(__name__)
 
@@ -99,6 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def websi():
 	return render_template("HOME.html")
+
+@app.route("/personHala")
+def websit():
+	return render_template("mywebsite.html")
+@app.route("/wow.html")
+def j():
+	return render_template("wow.html")
 
 @app.route("/his")
 def W():
@@ -109,8 +22,12 @@ def e():
 @app.route("/form")
 def f():
 	return render_template("forms.html")	
-@app.route("/vid")
 
+@app.route("/personSaja")
+def a():
+	return render_template("personal.html")	
+
+@app.route("/vid")
 def s():
 	return render_template("vid.html")
 @app.route("/submit", methods=["POST"])
